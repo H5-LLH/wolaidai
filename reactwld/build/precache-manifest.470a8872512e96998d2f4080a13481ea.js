eb141cd1c8d39874d27e57f242fc3a7655b39f69
qazrunning <920969072@qq.com>
2018-11-27 17:47:37 +0800

    11.0.27.1版本

---
+++ reactwld/build/precache-manifest.470a8872512e96998d2f4080a13481ea.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf6dfc9a588477909efd6b15248bb21",
+    "url": "/static/media/iphonexs1.1cf6dfc9.png"
+  },
+  {
+    "revision": "9072cf0a11ea67330410",
+    "url": "/static/css/main.d24f7f51.chunk.css"
+  },
+  {
+    "revision": "306ef8736a3ee48dad05e687021ac836",
+    "url": "/static/media/iconfont.306ef873.svg"
+  },
+  {
+    "revision": "736213c42c616a4ffe87",
+    "url": "/static/js/1.736213c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "17619874c8fa03c351fce6f6199824f3",
+    "url": "/static/media/banner-credit-card.17619874.png"
+  },
+  {
+    "revision": "d121c5cc65446ea13ff243836b1cb0b9",
+    "url": "/static/media/banner1.d121c5cc.png"
+  },
+  {
+    "revision": "e45e354070ecd49d810d1dbe240b2246",
+    "url": "/static/media/banner2.e45e3540.png"
+  },
+  {
+    "revision": "89dd424a715f051c723dd03dbe08c125",
+    "url": "/static/media/banner-aif-dsd.89dd424a.png"
+  },
+  {
+    "revision": "9bc821c28c51bc7d5cb937cfb8ff01c7",
+    "url": "/static/media/djd_banner.9bc821c2.png"
+  },
+  {
+    "revision": "1a81d0f0ef4a7c5ef4f6ac97b7eed9f6",
+    "url": "/static/media/operation-bit-banner.1a81d0f0.png"
+  },
+  {
+    "revision": "a6f397a2ab1fe695e8fea1566ffed725",
+    "url": "/static/media/hw_mate20_pro.a6f397a2.png"
+  },
+  {
+    "revision": "db240a17738cd9af01a4fe0734ea5ae6",
+    "url": "/static/media/hw_mate20.db240a17.png"
+  },
+  {
+    "revision": "ed68d0abd6010b346cca6a14797b325e",
+    "url": "/static/media/iphonexs_max2.ed68d0ab.png"
+  },
+  {
+    "revision": "9072cf0a11ea67330410",
+    "url": "/static/js/main.9072cf0a.chunk.js"
+  },
+  {
+    "revision": "c753228c55cf88127401fe2d294a8204",
+    "url": "/static/media/iphonexr2.c753228c.png"
+  },
+  {
+    "revision": "3d9f7f6f5f8edd6da6f12dfe75bce693",
+    "url": "/static/media/p1.3d9f7f6f.jpeg"
+  },
+  {
+    "revision": "166156f964a6b467bb478b4662e37dcb",
+    "url": "/static/media/p2.166156f9.jpeg"
+  },
+  {
+    "revision": "af45750f6507437d709712bc248519ac",
+    "url": "/static/media/p4.af45750f.png"
+  },
+  {
+    "revision": "f594d3da62886805f9d3d559fad548fa",
+    "url": "/static/media/p5.f594d3da.png"
+  },
+  {
+    "revision": "cc7d0e7f3906acaebe18c2df13279f98",
+    "url": "/static/media/p6.cc7d0e7f.jpeg"
+  },
+  {
+    "revision": "184597b7b6368bd8574e0e0e666bea4e",
+    "url": "/static/media/borrow.184597b7.png"
+  },
+  {
+    "revision": "6617ee7517c320759190c7e63a5b61f7",
+    "url": "/static/media/bg-dejdd1.6617ee75.jpg"
+  },
+  {
+    "revision": "e7739a9679a5fd05cf3b720f9071dc3b",
+    "url": "/static/media/button.e7739a96.png"
+  },
+  {
+    "revision": "311ad63b113656aa8f3f2a7de067e390",
+    "url": "/static/media/iconfont.311ad63b.eot"
+  },
+  {
+    "revision": "1f2997895d2f552394b4303eba756e82",
+    "url": "/static/media/iconfont.1f299789.ttf"
+  },
+  {
+    "revision": "736213c42c616a4ffe87",
+    "url": "/static/css/1.1406a823.chunk.css"
+  },
+  {
+    "revision": "d3d040896e6d52055fb8567823e1822c",
+    "url": "/index.html"
+  }
+];
